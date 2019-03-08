4bf6e7b8be28dfbd8037e2262acb2caeaee015cb
Berlsky <ccp@efanghang.com>
2019-03-08 15:48:21 +0800

    新建项目

---
+++ widget/src/main/java/com/berlski/custom/widget/InputInfoSwitchView.java
@@ -0,0 +1,22 @@
+package com.berlski.custom.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class InputInfoSwitchView extends LinearLayout {
+
+    public InputInfoSwitchView(Context context) {
+        super(context);
+    }
+
+    public InputInfoSwitchView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public InputInfoSwitchView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
